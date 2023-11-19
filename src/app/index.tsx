bb55a59902c87f4630ef7abe670e214ed93b27e5
RayFreedom88 <captain.musin@yandex.ru>
2023-11-19 22:32:49 +0200

    [feat] добавляет либу @grammyjs/web-app

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,14 @@
+import { useEffect } from "react";
+import { WebApp as tg } from "@grammyjs/web-app";
 import { Routing } from "pages";
 import { withProviders } from "./providers";
 import "./styles/index.scss";
 
 const App = () => {
+  useEffect(() => {
+    tg.ready()
+  }, [])
+
   return (
     <div className="app">
       <Routing />
