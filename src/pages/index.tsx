c389cffeb148287d79adb2b5009d871f499d3ee8
RayFreedom88 <captain.musin@yandex.ru>
2023-12-12 18:29:32 +0200

    [feat] Добавляет настройку для netlify и закрывает TODO

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { Navigate, Route, Routes } from "react-router-dom";
 
 const MainPage = lazy(() => import("./main"));
 const VacancyPage = lazy(() => import("./vacancy"));
-// TODO: Когда будет готова MVP приложения, нужно проверить роутинг. На данный момент после обновления страницы (на любой кроме главной) браузер говорит, что нет такого сайта. Возможно причина кроется в Netlify.
+
 export const Routing = () => {
   return (
     <Routes>
